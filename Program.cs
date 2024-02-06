f1589eac746a80795255b90d16cb3943e9a485d6
Anand Kunal Mishra <73222935+anandkunalmishra@users.noreply.github.com>
2024-02-06 09:50:12 +0530

    [Anand]: FileIO_starts

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace File
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //string path = @"/Users/anandkunalmishra/Desktop/untitled folder/file.txt";
+            string input = @"/Users/anandkunalmishra/Desktop/untitled folder/input.txt";
+            string output = @"/Users/anandkunalmishra/Desktop/untitled folder/output.txt";
+
+            using (StreamReader reader= new StreamReader(input))
+            {
+                using (StreamWriter writer = new StreamWriter(output))
+                {
+                    string[] arr = reader.ReadLine().Split();
+                    int x = int.Parse(arr[0]);
+                    int y = int.Parse(arr[1]);
+
+                    writer.WriteLine(x+y);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
