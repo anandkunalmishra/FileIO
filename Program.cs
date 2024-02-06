f965bab83d5993533bb84a2b3e86d8471acbe15d
Anand Kunal Mishra <73222935+anandkunalmishra@users.noreply.github.com>
2024-02-06 10:55:58 +0530

    [Anand]:CSV_Implementation_done

--- Program.cs
+++ Program.cs
@@ -7,18 +7,43 @@ namespace File
         public static void Main(string[] args)
         {
             //string path = @"/Users/anandkunalmishra/Desktop/untitled folder/file.txt";
-            string input = @"/Users/anandkunalmishra/Desktop/untitled folder/input.txt";
+            //string input = @"/Users/anandkunalmishra/Desktop/untitled folder/input.txt";
             string output = @"/Users/anandkunalmishra/Desktop/untitled folder/output.txt";
 
-            using (StreamReader reader= new StreamReader(input))
-            {
-                using (StreamWriter writer = new StreamWriter(output))
-                {
-                    string[] arr = reader.ReadLine().Split();
-                    int x = int.Parse(arr[0]);
-                    int y = int.Parse(arr[1]);
 
-                    writer.WriteLine(x+y);
+            string csvFile = @"/Users/anandkunalmishra/Downloads/Contacts.csv";
+            Dictionary<int,string> label= new Dictionary<int,string>();
+            using (StreamReader reader= new StreamReader(csvFile))
+            {
+                using(StreamWriter writer = new StreamWriter(output))
+                {
+                    string line;
+                    int counter = 0;
+
+                    int labelCounter = 0;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string [] words = line.Split(',');
+                        counter++;
+                        if (counter == 1)
+                        {
+                            foreach(var word in words)
+                            {
+                                label.Add(labelCounter,word);
+                                labelCounter++;
+                            }
+                        }
+                        else
+                        {
+                            labelCounter = 0;
+                            foreach (var word in words)
+                            {
+                                writer.Write(label[labelCounter] + " : " + word + ", ");
+                                labelCounter++;
+                            }
+                            writer.WriteLine();
+                        }
+                    }
                 }
             }
             Console.ReadLine();
